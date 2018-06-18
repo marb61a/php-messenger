0e83841fb9e00513926f5c96ef0da5ad6af34111
martin byrne <marb61a@gmail.com>
2018-06-18 20:19:47 +0000

    added initial signup and index php files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, intial-scale=1, shrink-to-fit=no">
+	    <title>Home</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, intial-scale=1, shrink-to-fit=no">
+	    <title>Create A New Account</title>
+    </head>
+    <body>
+        <div class="signup-container">
+            
+        </div>
+    </body>
+</html>
