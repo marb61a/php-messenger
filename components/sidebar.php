5e519c32d49d1b457aa39b82e6855376fedf9882
martin byrne <marb61a@gmail.com>
2018-06-19 11:58:56 +0000

    added initial sidebar and updated login.php

---
+++ components/sidebar.php
@@ -0,0 +1,14 @@
+<section id="sidebar">
+    <ul class="left-ul">
+        <li>
+            <a href="javascript:void(0);">
+                <span class="profile-img-span">
+                    <img>
+                </span>
+            </a>
+        </li>
+        <li>
+            
+        </li>
+    </ul>
+</section>
--- login.php
+++ login.php
@@ -1,5 +1,9 @@
-<?php 
-    include "init.php";
+<?php include "init.php"; ?>
+<?php
+    $obj = new base_class;
+    if(isset($_POST['login'])){
+        
+    }
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +15,21 @@
     </head>
     </head>
     <body>
+        <div class="signup-container">
+            <div class="account-left">
+                <div class="account-text">
+                    <h1>Let's Chat</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+   		                tempor incididunt ut labore mpor incididunt ut labore
+   		            </p>
+                </div>
+            </div> <!-- close account-left-->
+            <div class="account-right">
+                <?php include 'components/login_form.php'; ?>
+            </div>
+        </div><!-- close signup-container -->
         
+        <?php include 'components/js.php'; ?>
     </body>
 </html>
