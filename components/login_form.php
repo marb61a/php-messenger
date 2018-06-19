1d43d4993b47b77517361a94e2376b5447119e2b
martin byrne <marb61a@gmail.com>
2018-06-19 11:05:56 +0000

    added login form

---
+++ components/login_form.php
@@ -0,0 +1,33 @@
+<div class="form-area">
+    <?php if(isset($_SESSION['account_success'])): ?>
+        <div class = "alert alert-success">
+            <?php echo $_SESSION['account_success']; ?>
+        </div>
+    <?php endif; ?>
+    
+    <?php unset( $_SESSION['account_success']); ?>
+    
+    <form method="POST" action="">
+        <div class="group">
+            <h2 class="form-heading">User Login</h2>
+        </div> <!-- group -->
+        <div class="group">
+            <input type="email" name="email" class="control" placeholder="Enter Email..."
+                value=" <?php if(isset($email)): echo $email; endif;?> ">
+            <div class="name-error error">
+                <?php if(isset($email_error)): ?>
+                     <?php echo $email_error; ?>
+                <?php endif; ?>
+            </div>
+        </div> <!-- group -->
+        <div class="group">
+            <input type="password" name="password" class="control" placeholder="Create Password..." 
+                value="<?php if(isset($password)): echo $password; endif;?>">
+           <div class="name-error error">
+                <?php if(isset($password_error)): ?>
+                    <?php echo $password_error; ?>
+                <?php endif; ?>
+            </div>
+        </div> <!-- close group -->
+    </form>
+</div> <!-- close form-area -->
