c7343c28851c54587520adba60a312b86c765ddb
martin byrne <marb61a@gmail.com>
2018-06-21 11:26:14 +0000

    added sidebar.php

--- components/sidebar.php
+++ components/sidebar.php
@@ -3,12 +3,34 @@
         <li>
             <a href="javascript:void(0);">
                 <span class="profile-img-span">
-                    <img>
+                    <img src="assets/img/<?php echo $_SESSION['user_image'];?>" alt="Profile image" class="profile-img">
                 </span>
             </a>
         </li>
-        <li>
-            
-        </li>
+		<li><a href="index.php">
+		    <?php echo ucfirst($_SESSION['user_name']); ?>
+		    <span class="cool-hover"></span></a>
+		</li>
+		<li><a href="change_name.php">
+		    Change Name <span class="cool-hover"></span></a>
+		</li>
+		<li><a href="change_password.php">
+		    Change Password <span class="cool-hover"></span></a>
+		</li>
+		<li><a href="change_image.php">
+		    Change Photo <span class="cool-hover"></span></a>
+		</li>
+		<li>
+		    <a href="#">
+		        <span class="online_users"></span>
+			    <span class="cool-hover"></span>
+		    </a>
+		</li>
+		<li><a href="javascript:void(0);" class="clean">
+		    Clean History <span class="cool-hover"></span></a>
+		</li>
+		<li>
+		    <a href="logout.php">logout<span class="cool-hover"></span></a>
+		</li>
     </ul>
 </section>
