7b7a8bcd5df3e2c769288c9f55cd74e3961915db
martin byrne <marb61a@gmail.com>
2018-06-21 11:52:21 +0000

    updated base class and chat.js

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -5,14 +5,38 @@ $(document).ready(function(){
             if(send_message.length != ""){
                 $.ajax({
                     type: 'POST',
-                    url: '',
+                    url: 'ajax/send_message.php',
                     data: {send_message:send_message},
 				    dataType: 'JSON',
 				    success: function(feedback){
-				        
+				        if(feedback.status == "success"){
+				            $('.chat_form').trigger("reset");
+				            show_messages();
+				            $(".messages").animate({
+				                scrollTop: $(".messages")[0].scrollHeight
+				            }, 2000);
+				        }
 				    }
                 });
             }
         }    
     });
+    
+    // Show messages from the database
+    function show_messages(){
+        var msg = true;
+        $.ajax({
+            type : 'GET',
+   	        url : 'ajax/show_messages.php',
+   	        data : {'message': msg},
+   	        success: function(feedback){
+   	            $(".messages").html(feedback);
+   	        }
+        });
+    }
+    
+    show_messages();
+    $(".messages").animate({
+        scrollTop: $(".messages")[0].scrollHeight
+    }, 2000);
 });
--- classes/base_class.php
+++ classes/base_class.php
@@ -2,8 +2,22 @@
     class base_class extends db {
         private $Query;
         
-        public function Normal_Query(){
-            
+        public function Normal_Query($query, $param = null){
+            if(is_null($param)){
+                $this->Query = $this->con->prepare($query);
+                return $this->Query->execute();
+            } else {
+                $this->Query = $this->con->prepare($query);
+                return $this->Query->execute($param);
+            }
+        }
+        
+        public function Count_Rows(){
+      	    return $this->Query->rowCount();
+        }
+        
+        public function security($data){
+            return trim(strip_tags($data));
         }
     }
 ?>
