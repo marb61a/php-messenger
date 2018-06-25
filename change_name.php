7c8997a0287d216572298a774b760c51e240a68c
martin byrne <marb61a@gmail.com>
2018-06-25 10:54:01 +0000

    added chang_name.php

---
+++ change_name.php
@@ -0,0 +1,39 @@
+<?php include "init.php"; ?>
+<?php include "security.php"; ?>
+<?php
+    $obj = new base_class;
+    if(isset($_POST['change_name'])){
+        $user_name = $obj->security($_POST['user_name']);
+        $user_id = $_SESSION['user_id'];
+        
+        if(empty($user_name)){
+            $user_name_error = "Apologies but name is required";
+        } else {
+            if($obj->Normal_Query("UPDATE users  SET name = ? WHERE id = ?", [$user_name, $user_id])){
+                $obj->Create_Session("user_name", $user_name);
+    			$obj->Create_Session("name_updated", "Your name is succesfully udpated");
+    			header("location:index.php");
+            }
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, intial-scale=1, shrink-to-fit=no">
+    	<title>Home</title>
+        <?php include 'components/css.php'; ?>
+    </head>
+    <body>
+        <?php include 'components/nav.php'; ?>
+        <div class="chat-container">
+            <?php include 'components/sidebar.php'; ?>
+            <section id="right-area">
+                <?php include 'components/change_name_form.php'; ?>
+            </section>
+        </div>
+        <?php include 'components/js.php'; ?>
+    </body>
+</html>
