9007ea3c9838eed8af94a370015149f6bd13e618
martin byrne <marb61a@gmail.com>
2018-06-25 11:13:52 +0000

    updated chat.js and added send_message.php ajax file

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -38,51 +38,60 @@ $(document).ready(function(){
         });  
     });
     
-    // Display online users
-    function online_users(){
-        $.ajax({
-            type: 'GET',
-            url  : 'ajax/online_users.php',
-            dataType: 'JSON',
-            success: function(feedback){
-                if(feedback['usera'] == 1){
-                    $('.online_users').html("<span class='show-online'></span> "+"Only You");
-                } else {
-                    $('.online_users').html("<span class='show-online'></span>Online Users "+feedback['users']);
-                }
-            }
-        });
-    }
+    online_users();
+    setInterval(function(){
+       show_messages();
+       users_status();
+       online_users();
+    },1000);
     
-    // Check user login time
-    function users_status() {
-    	$.ajax({
-    		type : 'GET',
-    		url  : 'ajax/users_status.php',
-    		dataType : 'JSON',
-    		success : function(feedback) {
-    			if(feedback['status'] == "href"){
-    				window.location = "login.php";
-    			}
-    		}
-    	});
-    }
-    
-    // Show messages from the database
-    function show_messages(){
-        var msg = true;
-        $.ajax({
-            type : 'GET',
-   	        url : 'ajax/show_messages.php',
-   	        data : {'message': msg},
-   	        success: function(feedback){
-   	            $(".messages").html(feedback);
-   	        }
-        });
-    }
-    
-    show_messages();
-    $(".messages").animate({
-        scrollTop: $(".messages")[0].scrollHeight
-    }, 2000);
 });
+
+    
+// Display online users
+function online_users(){
+    $.ajax({
+        type: 'GET',
+        url  : 'ajax/online_users.php',
+        dataType: 'JSON',
+        success: function(feedback){
+            if(feedback['usera'] == 1){
+                $('.online_users').html("<span class='show-online'></span> "+"Only You");
+            } else {
+                $('.online_users').html("<span class='show-online'></span>Online Users "+feedback['users']);
+            }
+        }
+    });
+}
+
+// Check user login time
+function users_status() {
+	$.ajax({
+		type : 'GET',
+		url  : 'ajax/users_status.php',
+		dataType : 'JSON',
+		success : function(feedback) {
+			if(feedback['status'] == "href"){
+				window.location = "login.php";
+			}
+		}
+	});
+}
+
+// Show messages from the database
+function show_messages(){
+    var msg = true;
+    $.ajax({
+        type : 'GET',
+           url : 'ajax/show_messages.php',
+           data : {'message': msg},
+           success: function(feedback){
+               $(".messages").html(feedback);
+           }
+    });
+}
+
+show_messages();
+$(".messages").animate({
+    scrollTop: $(".messages")[0].scrollHeight
+}, 2000);
