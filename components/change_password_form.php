40089236bb817b1cc6a07e29293bb9d947f23dbd
martin byrne <marb61a@gmail.com>
2018-06-27 11:43:06 +0000

    Updated  change_password_form.php component

--- components/change_password_form.php
+++ components/change_password_form.php
@@ -5,8 +5,32 @@
                 <h2 class="form-heading">Change Password</h2>
             </div> <!-- group -->
             <div class="group">
-                <input type="password" name="current_password"/>
+                <input type="password" name="current_password" class="control" placeholder="Current Password"/>
+                <div class="name-error error">
+                    <?php if(isset($current_password_error)): ?>
+                        <?php echo $current_password_error; ?>
+                    <?php endif; ?>
+                </div>
+            </div> <!-- close group -->
+            <div class="group">
+                <input type="password" name="new_password" class="control" placeholder="Create New Password">
+                <div class="name-error error">
+                    <?php if(isset($new_password_error)): ?>
+                        <?php echo $new_password_error; ?>
+                    <?php endif; ?>
+                </div>
+            </div> <!-- close group -->
+            <div class="group">
+                <input type="password" name="retype_password" class="control" placeholder="Retype Password">
+                <div class="name-error error">
+                    <?php if(isset($retype_password_error)): ?>
+                        <?php echo $retype_password_error; ?>
+                    <?php endif; ?>
+                </div>
+            </div> <!-- close group -->
+            <div class="group">
+                <input type="submit" name="change_password" class="btn account-btn" value="Save Changes">
             </div>
         </form>
-    </div>
-</div>
+    </div> <!-- close form-grid -->
+</div> <!-- close form-section -->
