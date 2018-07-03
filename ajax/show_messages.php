4aaac7481acdc57f73dca6eeb1a3fab8e930fd04
martin byrne <marb61a@gmail.com>
2018-07-03 11:28:30 +0000

    updated validations in show messages

--- ajax/show_messages.php
+++ ajax/show_messages.php
@@ -201,7 +201,51 @@
 									</div><!-- close left-msg -->
 								</div><!-- close left-msg-area -->
 							</div><!-- close left-message -->';
-			            }
+			            } else if($msg_type == "zip" || $msg_type == "ZIP") {
+							echo '<div class="left-message common-margin">
+								<div class="sender-img-block">
+									<img src="assets/img/'.$user_image.'" class="sender-img">
+									'.$user_online_status.'
+								</div><!-- close sender-img-block -->
+								<div class="left-msg-area">
+									<div class="user-name-date">
+										<span class="sender-name">
+											'.$full_name.'
+										</span><!-- close sender-name -->
+										<span class="date-time">
+											'.$msg_time .'
+										</span><!-- close date-time -->
+									</div><!-- close user-name-date -->
+									<div class="left-files">
+					                     <a href="assets/img/'.$message.'" class="all-files">
+					                    	<i class="fas fa-arrow-circle-down files-common"></i>'.$message.'
+					                    </a>
+									</div><!-- close left-msg -->
+								</div><!-- close left-msg-area -->
+							</div><!-- close left-message -->';
+						} else if($msg_type == "PDF" || $msg_type == "pdf") {
+							echo '<div class="left-message common-margin">
+								<div class="sender-img-block">
+									<img src="assets/img/'.$user_image.'" class="sender-img">
+									'.$user_online_status.'
+								</div><!-- close sender-img-block -->
+								<div class="left-msg-area">
+									<div class="user-name-date">
+										<span class="sender-name">
+											'.$full_name.'
+										</span><!-- close sender-name -->
+										<span class="date-time">
+											'.$msg_time .'
+										</span><!-- close date-time -->
+									</div><!-- close user-name-date -->
+									<div class="left-files">
+                    					<a href="assets/img/'.$message.'" class="all-files" target="_blank">
+                    						<i class="far fa-file-pdf pdf files-common"></i>'.$message.'
+                    					</a>
+									</div><!-- close left-msg -->
+								</div><!-- close left-msg-area -->
+							</div><!-- close left-message -->';
+						}
 			        }
 			
 		        endforeach;   
--- classes/db.php
+++ classes/db.php
@@ -1,18 +1,41 @@
 <?php
-    $servername = getenv('IP');
-    $username = getenv('C9_USER');
-    $password = "";
-    $database = "c9";
+    // $servername = getenv('IP');
+    // $username = getenv('C9_USER');
+    // $password = "";
+    // $database = "c9";
+    
+    // class db {
+    // protected $con;    
+        
+    //     public function __construct() {
+    //         try {
+    //             $this->con = new PDO($this->servername, $this->database, $this->username, $this->password);
+    //         } catch(Exception $e){
+			 //   echo "DataBase Connection Problem: ". $e->getMessage();
+		  //  }
+    //     }
+    // }
     
     class db {
-    protected $con;    
-        
-        public function __construct() {
-            try {
-                $this->con = new PDO($this->servername, $this->database, $this->username, $this->password);
-            } catch(Exception $e){
-			    echo "DataBase Connection Problem: ". $e->getMessage();
-		    }
-        }
-    }
+
+
+	private $host     = "localhost";
+	private $dbname   = "messenger";
+	private $username = "root";
+	private $password = "";
+	protected $con;
+
+	public function __construct() {
+
+		try {
+			// new PDO("mysql:host=localhost;dbname=messenger","root","");
+			$this->con = new PDO("mysql:host=". $this->host. ";dbname=". $this->dbname, $this->username, $this->password);
+			
+		}
+		catch(Exception $e){
+			echo "DataBase Connection Problem: ". $e->getMessage();
+		}
+	}
+
+}
 ?>
